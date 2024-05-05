adc4e6a840220903f6cf50dd53a35ad3ceb91356
jovixd <jojovialxd@gmail.com>
2024-05-04 20:30:46 -0500

    add basepath

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const useSubDirectory = import.meta.env.PROD === 'true'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: useSubDirectory ? '/interactive-comic/' : '/',
   plugins: [react()],
 });
