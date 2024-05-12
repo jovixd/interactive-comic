bf1d8d71d3e57dee3be0d63d9483dfc49a61d855
jovixd <jojovialxd@gmail.com>
2024-05-12 03:28:11 -0500

    layout

--- src/pages/01.tsx
+++ src/pages/01.tsx
@@ -1,39 +1,49 @@
 import Container from "@mui/material/Container"
 import data from "../data/01.json"
-import { Box, Button } from "@mui/material"
+import { Box, Button, styled } from "@mui/material"
 import Grid from "@mui/material/Unstable_Grid2"
+
+const ActionButton = styled(Button)({
+    borderRadius: "3rem",
+    textTransform: 'none',
+    height: '6rem',
+    fontSize: '2rem',
+})
 
 type DuplicateButtonProps = {
     label: string,
     j: number
 }
 
-const DuplicateButton: React.FC<DuplicateButtonProps> = ({label, j}) => (
+const DuplicateButton: React.FC<DuplicateButtonProps> = ({ label, j }) => (
     <Grid display="flex" justifyContent="space-around" key={j} xs={6}>
-        <Button fullWidth size="large" variant="contained" color="primary" sx={{ borderRadius: "2rem", textTransform: 'none' }}>{label}</Button>
+        <ActionButton fullWidth disableElevation size="large" variant="contained" color="primary">{label}</ActionButton>
     </Grid>
 )
 
 const EscapeFromTheOceanPrison = () => {
     return (
-        <Container fixed disableGutters maxWidth="md">
+        <Container disableGutters maxWidth="md">
             {data.map((page, i) => {
                 return (
-                    <Grid container spacing={3} key={i}>
-                        <Grid xs={12} display="flex">
-                            <Box flexGrow={1} component="img" src={page.image} />
-                        </Grid>
-                        {page.actionData.map((action, j) => {
-                            return (
-                                <>
-                                    <DuplicateButton label={action.label} j={j}/>
-                                    <DuplicateButton label={action.label} j={j}/>
-                                    <DuplicateButton label={action.label} j={j}/>
-                                    <DuplicateButton label={action.label} j={j}/>
-                                </>
-                            )
-                        })}
-                    </Grid>
+                    <>
+                        <Box display="flex" flexDirection="column" key={i}>
+                            <Box component="img" src={page.image} />
+                            <Grid container spacing={3} key={i} disableEqualOverflow>
+                                {page.actionData.map((action, j) => {
+                                    return (
+                                        <>
+                                            <DuplicateButton label={action.label} j={j} />
+                                            <DuplicateButton label={action.label} j={j} />
+                                            <DuplicateButton label={action.label} j={j} />
+                                            <DuplicateButton label={action.label} j={j} />
+                                        </>
+                                    )
+                                })}
+                            </Grid>
+                        </Box>
+                    </>
+
                 )
             })}
         </Container>
