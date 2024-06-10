f59f65fedbb31055cccb99c5dbca71385bb572ee
jovixd <jojovialxd@gmail.com>
2024-06-09 21:02:14 -0700

    try removing router basepath

--- src/components/InteractiveComic.tsx
+++ src/components/InteractiveComic.tsx
@@ -73,7 +73,7 @@ const InteractiveComic: React.FC<InteractiveComicProps> = ({ pageData, currentFl
             <Box display="flex" flexDirection="column">
                 {loading && <Loader />}
                 {/* TODO: baseUrl appended to image for GH Pages */}
-                <Box component="img" src={pageData.image} onLoad={() => setLoading(false)} display={loading ? "none" : "block"} mb={4} alt={pageData.id}/>
+                <Box component="img" src={import.meta.env.BASE_URL + pageData.image} onLoad={() => setLoading(false)} display={loading ? "none" : "block"} mb={4} alt={pageData.id}/>
                 <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gap: '1em' }} m={2} mb={4}>
                     {pageData.actionData.map((action, index) => {
                         switch (action.type) {
