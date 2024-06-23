8251b23028a27a2b3a7ab709a87ae0611622863c
jovixd <jojovialxd@gmail.com>
2024-06-22 23:27:46 -0700

    dont add trailing slash to basename

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   const useGithubActions = process.env.VITE_USE_GITHUB_ACTIONS
-  console.log("GitHub Actions is", useGithubActions)
   return {
-    base: useGithubActions ? '/interactive-comic/' : '/',
+    base: useGithubActions ? '/interactive-comic' : '/',
     plugins: [react()]
   }
 });
