cb5632d25e95ed70f46fa01ae668873f1078a6ea
jovixd <jojovialxd@gmail.com>
2024-06-22 23:46:54 -0700

    additional printing

--- src/pages/01PrisonEscape.tsx
+++ src/pages/01PrisonEscape.tsx
@@ -1,3 +1,4 @@
+import Link from "@mui/material/Link"
 import ComicWrapper, { getPagesMap } from "../components/ComicWrapper"
 import data from "../data/01.json"
 import { Data } from "../types"
@@ -8,6 +9,10 @@ const PrisonEscape = () => {
     return (
         <>
             <span>{import.meta.env.BASE_URL}</span>
+            <br/>
+            <span>{import.meta.env.MODE}</span>
+            <br/>
+            <Link href="/02-isekai-quest">Test link to the next comic</Link>
             <ComicWrapper pages={getPagesMap(comicData.pages)} flags={comicData.flags} coverPage="P00" startPage="P01" />
         </>
     )
